96f5cb3b0053c0fa6977c82a7c317f2cde704d54
Volodymyr Selevertov <volodymyr@MacBook-Pro-Volodymyr.local>
2024-12-15 18:48:34 +0200

    rework header

--- src/components/AppHeader.js
+++ src/components/AppHeader.js
@@ -10,8 +10,6 @@ function AppHeader(){
                 </Navbar.Brand>
                 <Nav className="me-auto">
                     <Nav.Link href="/">Home</Nav.Link>
-                    <Nav.Link href="#features">Features</Nav.Link>
-                    <Nav.Link href="#pricing">Pricing</Nav.Link>
                 </Nav>
             </Container>
         </Navbar>
